eb15503e126ac723732f5a6170e0204bf9063300
Caramel-Cafe <168911469+Caramel-Cafe@users.noreply.github.com>
2024-05-13 12:51:14 +0300

    Create Style.js

---
+++ assets/css/Style.js
@@ -0,0 +1,9 @@
+function showDropdown() {
+  var dropdown = document.querySelector('.primary-navigation ul li ul');
+  dropdown.style.visibility = 'visible';
+}
+
+function hideDropdown() {
+  var dropdown = document.querySelector('.primary-navigation ul li ul');
+  dropdown.style.visibility = 'hidden';
+}
